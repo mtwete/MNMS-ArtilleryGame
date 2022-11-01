eba86d610dd5086234151a11f045c712efb63041
minavu <mina8@pdx.edu>
2022-11-01 15:33:01 -0700

    Updated paths in background test, added test to target test, added empty __init__.py to make test discovery work

--- tests/test_target.py
+++ tests/test_target.py
@@ -7,5 +7,8 @@ class TestTarget(unittest.TestCase):
         self.assertIsNotNone(target)
         self.assertGreater(target.points, 0)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_random_creation(self):
+        points_list = set(())
+        for i in range(10):
+            points_list.add(Target().points)
+        self.assertGreater(len(points_list), 1)
