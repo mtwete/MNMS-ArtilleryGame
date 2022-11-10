e1a3f67a1097ac181e01a8e19b12c6674210e4e3
minavu <mina8@pdx.edu>
2022-11-09 16:27:24 -0800

    Added menu class and control flow in game loop

--- menu.py
+++ menu.py
@@ -1,1 +1,9 @@
 from constants import *
+
+class Menu(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface(DISPLAY_SIZE)
+        self.image.fill('aqua')
+        self.rect = self.image.get_rect()
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from player import Player
 from missile import Missile
 from target import Target
 from background import Background
+from menu import Menu
 
 #initialize pygame, display, clock and target sprites
 pygame.init()
@@ -19,61 +20,73 @@ player_missile = []
 #Add a target sprite with random size
 shootingTarget = Target()
 targetSprites.add(shootingTarget)
+
+menu = Menu()
+game_play = False
 while True:
-    #background
-    #set up background object
-    background = Background(BACKGROUND_IMAGES_FILE_PATHS)
-    #display the background image underneath everything else
-    display.blit(background.image,background.loc)
-    #Display the current score of the player
-    player.display_score(display)
+    if not game_play:
+        display.blit(menu.image, (0, 0))
 
-    #get mouse click position
-    mouse_x , mouse_y = pygame.mouse.get_pos()
+        #check for events in game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                pygame.QUIT
+    else:
+        #background
+        #set up background object
+        background = Background(BACKGROUND_IMAGES_FILE_PATHS)
+        #display the background image underneath everything else
+        display.blit(background.image,background.loc)
+        #Display the current score of the player
+        player.display_score(display)
 
-    #check for events in game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            pygame.QUIT
+        #get mouse click position
+        mouse_x , mouse_y = pygame.mouse.get_pos()
 
-        #bullet clicks    
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                player_missile.append(Missile(player.x, player.y, mouse_x, mouse_y))
+        #check for events in game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                pygame.QUIT
 
-    #tank movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        player.x -= 2
-    if keys[pygame.K_d]:
-        player.x += 2
-    if keys[pygame.K_w]:
-        player.y-= 2
-    if keys[pygame.K_s]:
-        player.y+= 2
+            #bullet clicks    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    player_missile.append(Missile(player.x, player.y, mouse_x, mouse_y))
 
-    #dont go off screen
-    if player.x <= 0:
-        player.x = 0
-    if player.x >= 766:
-        player.x = 766
-    if player.y <= 0:
-        player.y = 0
-    if player.y >= 566:
-        player.y = 566 
+        #tank movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player.x -= 2
+        if keys[pygame.K_d]:
+            player.x += 2
+        if keys[pygame.K_w]:
+            player.y-= 2
+        if keys[pygame.K_s]:
+            player.y+= 2
 
-    #display tank, bullets and targets
-    player.main(display)
-    targetSprites.draw(display)
-    for bullet in player_missile:
-        bullet.main(display)
-        points = shootingTarget.update(bullet)
-        if points is not None:
-            player.update_score(points)
-        if len(targetSprites.sprites()) == 0:
-            shootingTarget = Target()
-            targetSprites.add(shootingTarget)
+        #dont go off screen
+        if player.x <= 0:
+            player.x = 0
+        if player.x >= 766:
+            player.x = 766
+        if player.y <= 0:
+            player.y = 0
+        if player.y >= 566:
+            player.y = 566 
+
+        #display tank, bullets and targets
+        player.main(display)
+        targetSprites.draw(display)
+        for bullet in player_missile:
+            bullet.main(display)
+            points = shootingTarget.update(bullet)
+            if points is not None:
+                player.update_score(points)
+            if len(targetSprites.sprites()) == 0:
+                shootingTarget = Target()
+                targetSprites.add(shootingTarget)
 
     #60 fps
     clock.tick(60)
