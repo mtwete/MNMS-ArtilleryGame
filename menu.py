302b3704ceccef20d7968cd2f7b7dc5727d135aa
minavu <mina8@pdx.edu>
2022-11-11 13:07:14 -0800

    Created 3d button look with text

--- menu.py
+++ menu.py
@@ -3,16 +3,23 @@ from constants import *
 class Button(pygame.sprite.Sprite):
     def __init__(self, button_text, center_x, center_y):
         super().__init__()
-        font = pygame.font.SysFont("arial", 30)
-        self.text_rendered = font.render(button_text, True, 'white')
-        self.text = button_text
+        self.value = button_text
 
-        self.image = pygame.Surface((self.text_rendered.get_width() + 20, self.text_rendered.get_height() + 10))
-        self.image.fill('black')
-        self.rect = self.image.get_rect(centerx = center_x, centery = center_y)
+        #create button surface
+        self.image = pygame.Surface((250, 60), pygame.SRCALPHA)
+        self.rect = self.image.get_rect(centerx=center_x, centery=center_y)
+        
+        #draw button box and shadow
+        self.button = pygame.draw.rect(self.image, 'white', pygame.Rect(0, 0, 247, 55), border_radius=15)
+        self.shadow = pygame.draw.rect(self.image, 'black', pygame.Rect(3, 5, 247, 55), border_radius=15)
 
-        self.textpos = self.text_rendered.get_rect(centerx = self.rect.width / 2, centery = self.rect.height / 2)
-        self.image.blit(self.text_rendered, self.textpos)
+        #add text on button
+        self.text = pygame.font.SysFont("arialblack", 30).render(self.value, True, 'black')
+        self.textpos = self.text.get_rect(centerx=self.button.width/2, centery=self.button.height/2)
+        self.image.blit(self.text, self.textpos)
+
+    def click_engage(self):
+        self.button = self.shadow
 
 
 
