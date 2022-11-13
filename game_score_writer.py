1d41dd1bbd9f419d768d302ff207808d349ab824
mtwete <matthew.twete@gmail.com>
2022-11-13 15:30:23 -0800

    Changed the class name of game_score_writer.py and game_score_reader.py to make the file name, added comments to game_score_writer.py and game_score_reader.py

--- game_score_writer.py
+++ game_score_writer.py
@@ -1,17 +1,21 @@
 
 # Class to write a gameScoreManager object to a file
-class GameScoreWriter:
-
+class ScoreFileWriter:
+    # file_path = file path to the document you want to write the leaderboard score data to
+    # game_scores = GameScoreManager object that contains the leaderboard score data
     def __init__(self, file_path, game_scores):
         self.scoreFile = file_path
         self.gameScores = game_scores
 
+    # Function to write the leaderboard score data from a GameScoreManager and store it in a file
     def write_scores(self):
+        # Open the file
         file_writer = open(self.scoreFile, "w")
-        # Doing this just so the last call to write doesn't add a newline to the end
-        # of the file
+        # Loop over the scores in the GameScoreManager object
         num_scores = len(self.gameScores.scores)-1
         for i, score in enumerate(self.gameScores.scores):
+            # Doing this check just so the last call to write doesn't add a newline to the end
+            # of the file
             if i != num_scores:
                 file_writer.write(score.to_string+'\n')
             else:
--- score_file_reader.py
+++ score_file_reader.py
@@ -1,19 +1,23 @@
-
-# Data class for a player's overall game score
-# Class to read a file with the leaderboard players/scores and store in a gameScoreManager
 from game_score import GameScore
 from game_score_manager import GameScoreManager
 
-
-class GameScoreReader:
+# Class to read a file with the leaderboard players/scores and store in a gameScoreManager
+class ScoreFileReader:
+    # file_path = file path to the document containing the leaderboard score data
     def __init__(self, file_path):
         self.scoreFile = file_path
 
+    # Function to read the leaderboard score data from a file and store it in a GameScoreManager object
     def read_scores(self):
+        # Try to open the file and read in the contents
         try:
+            # Open the file
             scoreboard_file = open(self.scoreFile, 'r')
+            # Get the lines
             lines = scoreboard_file.readlines()
             game_score_manager = GameScoreManager()
+            # Loop over the lines, splitting the data to feed into a GameScore object, then add that object
+            # to the game score manager
             for line in lines:
                 fields = line.split()
                 game_score_manager.add_score(GameScore(int(fields[1]),fields[0]))
