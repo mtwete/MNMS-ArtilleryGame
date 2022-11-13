81ab30344b397d1c3f2f635277a03a2612f49609
mtwete <matthew.twete@gmail.com>
2022-11-13 12:33:20 -0800

    Updated the test_game_score unit test file

--- tests/test_game_score.py
+++ tests/test_game_score.py
@@ -2,24 +2,27 @@ import unittest
 from game_score import GameScore
 
 
-class MyTestCase(unittest.TestCase):
-
+class TestGameScore(unittest.TestCase):
+    # Test the constructor
     def test_constructor(self):
         game_score = GameScore(1132, "Andre")
         self.assertIsNotNone(game_score)
         self.assertEqual(game_score.score, 1132)
         self.assertEqual(game_score.player_name, "Andre")
 
+    # Test the to_string method
     def test_tostring(self):
         game_score = GameScore(1132, "Andre")
         expected_string = "Andre" + "\t" + str(1132)
         self.assertEqual(expected_string, game_score.to_string())
 
+    # Make sure the equality operator works
     def test_equality(self):
         game_score1 = GameScore(1132, "Andre")
         game_score2 = GameScore(1132, "Jess")
         self.assertTrue(game_score1 == game_score2)
 
+    # Make sure the < operator works
     def test_less_than(self):
         game_score1 = GameScore(1132, "Andre")
         game_score2 = GameScore(2132, "Jess")
