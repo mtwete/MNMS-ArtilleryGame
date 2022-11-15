b01364be0060b01a5552160a35627e8358754cdc
mtwete <matthew.twete@gmail.com>
2022-11-15 12:55:35 -0800

    Added asserts to the test_equality and test_less_than tests to make sure it works when the objects aren't equal or aren't less than too

--- tests/test_game_score.py
+++ tests/test_game_score.py
@@ -21,12 +21,16 @@ class TestGameScore(unittest.TestCase):
         game_score1 = GameScore(1132, "Andre")
         game_score2 = GameScore(1132, "Jess")
         self.assertTrue(game_score1 == game_score2)
+        game_score3 = GameScore(1133, "Andre")
+        game_score4 = GameScore(1132, "Jess")
+        self.assertFalse(game_score3 == game_score4)
 
     # Make sure the < operator works for scores
     def test_less_than(self):
         game_score1 = GameScore(1132, "Andre")
         game_score2 = GameScore(2132, "Jess")
         self.assertTrue(game_score1 < game_score2)
+        self.assertFalse(game_score2 < game_score1)
 
 
 
