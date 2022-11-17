2a5bfde2ac782430d87781d4621c6e3dcd222ec9
minavu <mina8@pdx.edu>
2022-11-16 19:13:13 -0800

    Tested button hover and click effects

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,6 +1,6 @@
 import unittest
 import pygame
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from button import Button
 
 class TestButton(unittest.TestCase):
@@ -24,5 +24,29 @@ class TestButton(unittest.TestCase):
         self.test_button.update()
         self.assertEqual(self.test_button.shadow, self.test_button.button)
 
-    def test_hover_effect(self):
-        pass
+    def test_hover_on_off_effects(self):
+        self.assertIs(self.test_button.button_color, "white")
+
+        pygame.mouse = MagicMock()
+        pygame.mouse.get_pos.return_value = self.test_button.rect.topleft
+        pygame.mouse.get_pressed.return_value = [False, False, False]
+        self.test_button.check_click()
+        self.assertIs(self.test_button.button_color, "gray")
+
+        pygame.mouse.get_pos.return_value = (self.test_button.rect.x - 1, self.test_button.rect.y - 1)
+        self.test_button.check_click()
+        self.assertIs(self.test_button.button_color, "white")
+
+    def test_click_on_off_effects(self):
+        self.assertFalse(self.test_button.clicked)
+        
+        pygame.mouse = MagicMock()
+        pygame.mouse.get_pos.return_value = self.test_button.rect.topleft
+        pygame.mouse.get_pressed.return_value = [True, False, False]
+        self.test_button.check_click()
+        self.assertTrue(self.test_button.clicked)
+
+        pygame.mouse.get_pressed.return_value = [False, False, False]
+        returned_value = self.test_button.check_click()
+        self.assertFalse(self.test_button.clicked)
+        self.assertEqual(returned_value, self.test_button.value)
