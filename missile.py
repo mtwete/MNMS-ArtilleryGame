c07fbc3764192a19c8adb96401bf1d94ad6128d7
minavu <79536485+minavu@users.noreply.github.com>
2022-11-23 17:21:45 -0800

    Revert "Explosion"

--- missile.py
+++ missile.py
@@ -1,11 +1,14 @@
-from constants import *
+import pygame
+import math
 
-class Missile(pygame.sprite.Sprite):
+class Missile:
     def __init__(self, x, y, mouse_x, mouse_y):
-        super().__init__()
-        self.image = pygame.Surface((4, 4), pygame.SRCALPHA)
-        self.rect = self.image.get_rect(center=(x, y))
-        self.color = 'white'
+        #location
+        self.x = x
+        self.y = y
+        #mouse clicks
+        self.mouse_x = mouse_x
+        self.mouse_y = mouse_y
         #speed of missile
         self.speed = 7
         #math that makes it work
@@ -14,6 +17,7 @@ class Missile(pygame.sprite.Sprite):
         self.y_vel = math.sin(self.angle) * self.speed
         
     #display on screen
-    def update(self):
-        self.rect.move_ip(-int(self.x_vel), -int(self.y_vel))
-        pygame.draw.circle(self.image, self.color, self.image.get_rect().center, 2)
+    def main(self, display):
+        self.x -= int(self.x_vel)
+        self.y -= int(self.y_vel)
+        pygame.draw.circle(display, (255, 255, 255), (self.x, self.y), 2)
