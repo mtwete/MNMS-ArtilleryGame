6c7fac77545e97cba02d35f63103da04f626da1f
mtwete <matthew.twete@gmail.com>
2022-11-28 12:27:48 -0800

    Added comments to class files

--- leaderboard.py
+++ leaderboard.py
@@ -5,36 +5,48 @@ from button import Button
 
 
 class Leaderboard(pygame.sprite.Sprite):
-
+    #Constructor
+    #leaderboard_scores = string with leaderboard scores from the game_score_list
+    #leaderboard_string method
     def __init__(self, leaderboard_scores):
         super().__init__()
         self.leaderboard_scores = leaderboard_scores
 
+        #Set up background
         self.image = Background(BACKGROUND_IMAGES_FILE_PATHS).image.convert()
         self.rect = self.image.get_rect()
+        #Display the leaderboard title text
         self.text = pygame.font.SysFont("arialblack", 50).render(LEADERBOARD_TITLE, True, 'red')
         self.textpos = self.text.get_rect(centerx=self.rect.width / 2, centery=self.rect.height / 10)
         self.image.blit(self.text, self.textpos)
 
-
+        #Generate text display for actual scores from LeaderboardLineWriter
         self.score_line_writer = LeaderboardLineWriter(self.leaderboard_scores, self.rect.width,self.rect.height,self.text.get_rect().height)
         self.image.blit(self.score_line_writer.image, self.score_line_writer.rect)
 
+        #Copy the original menu
         self.image_original = self.image.copy()
 
+        #Set up return to menu and exit game buttons
         self.return_button = Button(RETURN_TO_MENU, center_x=self.rect.width / 2, center_y=self.rect.height / 2 + 160)
         self.exit_button = Button(EXIT_GAME, center_x=self.rect.width / 2, center_y=self.rect.height / 2 + 240)
         self.button_group = pygame.sprite.Group()
         self.button_group.add(self.return_button, self.exit_button)
 
-
+    #Method to draw the leaderboard screen
     def draw(self):
         self.image.blit(self.image_original, self.rect)
         self.button_group.update()
         self.button_group.draw(self.image)
 
+    #Method to check for button clicks
     def check_button_click(self):
+        #Since run.py goes to the main menu logic when game state is None
+        #if the user clicks the return button and it will return the return button
+        #string, so return None instead so the game state will make the main menu logic
+        #to run
         if self.return_button.check_click() is not None:
             return None
+        #Check for exit button click, if there's nothing stay at the leaderboard
         return self.exit_button.check_click() or LEADER_BOARD
 
--- leaderboard_line_writer.py
+++ leaderboard_line_writer.py
@@ -1,7 +1,14 @@
 from constants import *
 
 class LeaderboardLineWriter(pygame.sprite.Sprite):
-
+    # Constructor
+    # leaderboard_scores = string with leaderboard scores from the game_score_list leaderboard_string method
+    # background_rect_width = leaderboard screen background image rect width
+    # background_rect_height = leaderboard screen background image rect height
+    # leaderboard_title_rect_height = leaderboard title text rect height
+    # font = text font
+    # size = text size
+    # width = line width of the leaderboard scores
     def __init__(self, leaderboard_scores,background_rect_width,background_rect_height,
                  leaderboard_title_rect_height, font='ariel', size=24, width=500):
         super().__init__()
@@ -9,32 +16,47 @@ class LeaderboardLineWriter(pygame.sprite.Sprite):
         self.font = pygame.font.SysFont(font, size)
         self.size = size
         self.width = width + 100
+        # Font color
         self.color = 'white'
+        # height of lines
         self.line_height = size + 6
         self.line_width = width
+        # Number of lines in the leaderboard
         self.line = 0
+        # Lines of leaderboard scores
         self.leaderboard_score_lines = []
         self.background_rect_width = background_rect_width
         self.background_rect_height = background_rect_height
         self.leaderboard_title_rect_height = leaderboard_title_rect_height
         self.update()
 
+    # Update the leaderboard
     def update(self):
         self.render_all_lines()
+        # Set up surface to blit the scores on and blit scores
         self.image = pygame.Surface((self.background_rect_width, self.background_rect_height), pygame.SRCALPHA)
         self.rect = self.image.get_rect()
         self.image.blits(self.leaderboard_score_lines)
 
+    # Render all the lines in the leaderboard score string
     def render_all_lines(self):
+        # Split the score string into lines
         score_lines = self.leaderboard_scores_text.splitlines()
+        # Render each line
         for line in score_lines:
             self.render_score_line(line)
 
+    # Render an individual score line
     def render_score_line(self, line):
+        # The lines have tabs separating each value which pygame doesn't line,
+        # so replace the tabs with 4 spaces
         reformatted_line = line.replace('\t', ' ' * 4)
+        # Render the line
         score_line = self.font.render(reformatted_line, True, 'white')
+        # Set up proper position, so they are space right
         position = score_line.get_rect()
         position.centerx = self.background_rect_width / 2
         position.y = (self.background_rect_height / 10)+(self.leaderboard_title_rect_height / 2)+self.line*self.line_height
+        # Add the score line to the list of the score lines
         self.leaderboard_score_lines.append((score_line, position))
         self.line += 1
