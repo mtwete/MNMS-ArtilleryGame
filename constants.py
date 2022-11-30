caa31a38d36200e3846fec0e6bba369ae10c6a46
mtwete <matthew.twete@gmail.com>
2022-11-29 18:23:54 -0800

    Updated constants.py to have the static variables needed for the leaderboard but still have everything it has in the main branch

--- constants.py
+++ constants.py
@@ -2,20 +2,24 @@ import os
 import pygame
 import sys
 import random
+import math
 
 DISPLAY_SIZE = (800, 600)
 TIMER_SECONDS = 100
 
-MUSIC_VOLUME_PERCENTAGE = .00
+MUSIC_VOLUME_PERCENTAGE = .03
 MUSIC_FILE_PATH = 'resources/Disco_Heavy.mp3'
-LEADERBOARD_SCORE_FILE_PATH = 'resources/leaderboard_scores.txt'
-LEADERBOARD_TITLE = 'TOP SCORES:'
 
 
 MAIN_DIR = os.path.dirname(__file__)
 IMAGES_DIR = os.path.join(MAIN_DIR, "resources/images")
+TANKS_DIR = os.path.join(MAIN_DIR, "resources/images/tanks")
 
 TARGET_IMAGE = os.path.join(IMAGES_DIR, "target.png")
+TANK_LEFT = os.path.join(TANKS_DIR, "tank_left.png")
+TANK_UP =os.path.join(TANKS_DIR, "tank_up.png")
+TANK_RIGHT = os.path.join(TANKS_DIR, "tank_right.png")
+TANK_DOWN = os.path.join(TANKS_DIR, "tank_down.png")
 
 #static variable holding the list of background image file paths
 #with the first being the main menu image and the rest being the images for the
@@ -24,11 +28,23 @@ MENU_BACKGROUND = os.path.join(IMAGES_DIR, 'Menu-Background-Resized.jpg')
 HEX_BACKGROUND = os.path.join(IMAGES_DIR, 'Hex-Background-Resized.jpg')
 BACKGROUND_IMAGES_FILE_PATHS = [MENU_BACKGROUND, HEX_BACKGROUND]
 
+#Leaderboard static variables
+LEADERBOARD_SCORE_FILE_PATH = os.path.join(MAIN_DIR, "resources/leaderboard_scores.txt")
+LEADERBOARD_TITLE = 'TOP SCORES:'
+
 RED_VIOLET = (202, 22, 142)
 CLAIRVOYANT = (100, 14, 110)
 
 START_GAME = "Start Game"
 LEADER_BOARD = "Leader Board"
 EXIT_GAME = "Exit Game"
-RETURN_TO_MENU = "Main Menu"
-ENTER = "ENTER"
+MAIN_MENU = "Main Menu"
+
+def create_sprite_groups(number_of_groups: int = 1):
+    if number_of_groups == 1:
+        return pygame.sprite.Group()
+
+    groups = []
+    for it in range(number_of_groups):
+        groups.append(pygame.sprite.Group())
+    return groups
